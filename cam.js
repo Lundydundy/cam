24870a1ed884f0fd111429e46ed36bdec0bf279b
Matteo Giliotti <matteogiliotti@gmail.com>
2024-03-24 21:16:28 -0400

    capture added

---
+++ cam.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", async () => {
+    const image = document.getElementById("image");
+    const res = document.getElementById("result");
+    const allergies = ["wheat", "barley", "rye", "gluten"]
+    const worker = await Tesseract.createWorker();
+
+    image.onchange = async () => {
+        let found = [];
+        console.log("change")
+        document.querySelector("#picture").src = URL.createObjectURL(image.files[0])
+        const result = await worker.recognize(image.files[0]);
+        const imageWords = result.data.text.toLowerCase();
+        console.log(imageWords)
+
+        allergies.forEach((allergy) => {
+            imageWords.includes(allergy) ? found.push(allergy) : ""
+        })
+
+        console.log(found)
+
+        res.value = found.length > 0 ? `Item has or may have ${found}.`: "No Allergies";
+    }
+})
