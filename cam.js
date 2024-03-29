365da7b7e9f60b1ab6702a84c8e0a940d7fcb7f1
Matteo Giliotti <matteogiliotti@gmail.com>
2024-03-29 13:48:30 -0400

    Trie implementation

--- cam.js
+++ cam.js
@@ -3,20 +3,98 @@ document.addEventListener("DOMContentLoaded", async () => {
     const res = document.getElementById("result");
     const allergies = ["wheat", "barley", "rye", "gluten"]
     const worker = await Tesseract.createWorker();
+    const trie = new Trie()
     
     image.onchange = async () => {
-        console.log(image.files)
+        console.time()
         document.querySelector("#picture").src = URL.createObjectURL(image.files[0])
         
         const result = await worker.recognize(image.files[0], {
             oem: 1
         });
         
-        const imageWords = result.data.text.toLowerCase();
-        const found = allergies.filter(allergy => imageWords.includes(allergy));
+        // const imageWords = result.data.words.map((word) => {
+        //     return word.text.toLowerCase()
+        // })
+
+        for(let i = 0; i < result.data.words.length; i++) {
+            trie.insert(result.data.words[i].text.toLowerCase())
+        }
+        // result.data.words.forEach(word => {
+        //     trie.insert(word.text.toLowerCase())
+        // })
+
+        // const found = allergies.filter(allergy => imageWords.includes(allergy));
+        const found = allergies.filter(allergy => trie.search(allergy));
+        
         res.value = found.length > 0 ? `Item has or may have ${found}.` : "No Allergies";
+        console.timeEnd()
     }
 })
+
+class Trie {
+    constructor() {
+        this.children = {};
+    }
+
+    insert(word) {
+        let curr = this.children;
+        for (let i = 0; i < word.length; i++) {
+            if (curr[word[i]]) {
+                curr = curr[word[i]];
+            } else {
+                curr = curr[word[i]] = {};
+            }
+        }
+        curr["isWord"] = true;
+    }
+
+    search(word) {
+        let curr = this.children;
+        for (let i = 0; i < word.length; i++) {
+            if (curr[word[i]]) {
+                curr = curr[word[i]];
+            } else {
+                return false;
+            }
+        }
+        return curr["isWord"] ? true : false;
+    }
+}
+
+
+// var Trie = function () {
+//     this.children = {}
+// };
+
+// Trie.prototype.insert = function (word) {
+//     let curr = this.children
+//     for (let i = 0; i < word.length; i++) {
+//         if (curr[word[i]]) {
+//             curr = curr[word[i]];
+//         } else {
+//             curr = curr[word[i]] = {};
+//             // curr = curr[word[i]];
+//         }
+//     }
+//     curr["isWord"] = true;
+// };
+
+
+// Trie.prototype.search = function (word) {
+//     let curr = this.children;
+//     for (let i = 0; i < word.length; i++) {
+//         if (curr[word[i]]) {
+//             curr = curr[word[i]]
+//         }
+//         else {
+//             return false;
+//         }
+//     }
+//     return curr["isWord"] ? true : false
+// };
+
+
 
 // document.addEventListener("DOMContentLoaded", async () => {
 //     const image = document.getElementById("image");
