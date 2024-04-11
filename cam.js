e3789361c2f58366e507bec5dfda4c99c7e092ce
Matteo Giliotti <matteogiliotti@gmail.com>
2024-04-11 11:24:00 -0400

    trie data changed

--- cam.js
+++ cam.js
@@ -3,21 +3,24 @@ document.addEventListener("DOMContentLoaded", async () => {
     const res = document.getElementById("result");
     const allergies = ["wheat", "barley", "rye", "gluten", "milk"]
     const worker = await Tesseract.createWorker();
+    const trie = new Trie()
+
+    allergies.forEach((allergy) => {
+        trie.insert(allergy);
+    })
 
     image.onchange = async () => {
-        const trie = new Trie()
+
         if(document.querySelector(".canvasImg")){
             document.querySelector(".canvasImg").remove()
         }
+
         document.querySelector("#picture").style.display = "block";
         const canvas = document.createElement('canvas');
         canvas.className = "canvasImg"
         canvas.style.maxWidth = "300px";
         canvas.style.maxHeight = "400px";
 
-        document.querySelectorAll(".bounding-box").forEach((element) => {
-            element.remove();
-        })
         document.querySelector("#picture").src = URL.createObjectURL(image.files[0]);
         const imgfile = document.createElement('img')
         imgfile.src = URL.createObjectURL(image.files[0]);
@@ -30,18 +33,23 @@ document.addEventListener("DOMContentLoaded", async () => {
             tessedit_char_blacklist: '0123456789!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~', // Ignore numbers and special characters
             oem: 1
         });
-    
-        for (let i = 0; i < result.data.words.length; i++) {
-            trie.insert(result.data.words[i].text.toLowerCase(), result.data.words[i].bbox)
-        }
-        const found = allergies.filter(allergy => trie.search(allergy)[0]);
-        const boxes = []
-        allergies.forEach((allergy) => {
-            const data = trie.search(allergy);
-            data[0] ? boxes.push(data[1]) : null;
-        })
+        
+        // const found = [];
+        const found = new Set()
+        const boxes = [];
+        // const data = {};
 
-        console.log(boxes);
+        for (let i = 0; i < result.data.words.length; i++) {
+            const word = result.data.words[i].text.toLowerCase()
+            if(trie.search(word)){
+                boxes.push(result.data.words[i].bbox);
+                found.add(word);
+                // if(!data[word]){
+                //     data[word] = i;
+                //     found.push(word);
+                // } 
+            }
+        }
 
         document.querySelector(".loader").style.display = "none"
         const cvimg = cv.imread("fileimg");
@@ -49,13 +57,13 @@ document.addEventListener("DOMContentLoaded", async () => {
             const x0 = box.x0, y0 = box.y0, x1 = box.x1, y1 = box.y1;
             const point1 = new cv.Point(x0, y0);
             const point2 = new cv.Point(x1, y1);
-            cv.rectangle(cvimg, point1, point2, new cv.Scalar(255,0,0, 255), 5);
+            cv.rectangle(cvimg, point1, point2, new cv.Scalar(255,0,0, 255), 3);
             document.querySelector("#picture").style.display = "none";
             document.querySelector(".image-div").append(canvas);
             cv.imshow(canvas, cvimg)
         });
 
-        res.innerHTML = found.length > 0 ? `Item has or may have${found.map((word) => ` ${word}`)}.` : "No Allergies";
+        res.innerHTML = found.size > 0 ? `Item has or may have ${Array.from(found).map((allergy) => ` ${allergy}`)}.` : "No Allergies";
         imgfile.remove();
     }
 })
@@ -66,7 +74,7 @@ class Trie {
         this.children = {};
     }
 
-    insert(word, box) {
+    insert(word) {
         let curr = this.children;
         for (let i = 0; i < word.length; i++) {
             if (curr[word[i]]) {
@@ -76,7 +84,6 @@ class Trie {
             }
         }
         curr["isWord"] = true;
-        curr["box"] = box;
     }
 
     search(word) {
@@ -88,7 +95,7 @@ class Trie {
                 return false;
             }
         }
-        return curr["isWord"] ? [true, curr.box] : false;
+        return curr["isWord"];
     }
 }
 
